5dcaba3875174ddfcf48c02c7925dc7ed71d7a7b
dordje21 <58385812+dordje21@users.noreply.github.com>
2019-12-03 12:54:30 +0200

    first commit

---
+++ com.gmail.lesson2.4b/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("введите высоту");
+		int h = sc.nextInt();
+
+		System.out.println("введите ширину");
+		int w = sc.nextInt();
+		for (int i = 1; i <= h; i++) {
+			for (int j = 1; j <= w; j++) {
+				if (i == 1 || i == h || j == 1 || j == w || i == j  || j == w + 1 - i) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+
+			}
+			System.out.println("");
+		}
+		sc.close();
+	}
+
+}
