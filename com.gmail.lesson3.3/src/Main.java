a0af92aff83868ed159639b2dbd5afe71be39581
dordje21 <58385812+dordje21@users.noreply.github.com>
2019-12-20 14:35:28 +0200

    all

--- com.gmail.lesson3.3/src/Main.java
+++ com.gmail.lesson3.3/src/Main.java
@@ -5,15 +5,21 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] students = new String[15];
-		String st1 = "Andrey";
-		String st2 = "Vasy";
-		students[5] = "Vasy";
 
+		String st1 = "Andrey";
+		students[5] = "Vasy";
+		students[7] = "Kate";
+		students[11] = "Maks";
+		students[14] = "Masha";
 		addStudent(students, st1);
 		del(students, "Vasy");
 		print(students);
+		contains(students, "Andrey");
+		sortStudent(students);
 
 		System.out.println(Arrays.toString(students));
+		
+		clear(students);
 	}
 
 	public static void addStudent(String[] string, String st) {
@@ -33,6 +39,7 @@ public class Main {
 				System.out.print(string[i]);
 			}
 		}
+		System.out.println();
 	}
 
 	public static void del(String[] string, String st) {
@@ -42,7 +49,43 @@ public class Main {
 			}
 		}
 	}
-	
-	
+
+	public static void contains(String[] string, String st) {
+		for (int i = 0; i < string.length; i++) {
+			if (string[i] == st) {
+				System.out.println(st + " есть в группе");
+			}
+		}
+	}
+
+	public static void sortStudent(String[] string) {
+		int sum = 0;
+		for (int i = 0; i < string.length; i++) {
+			if (string[i] != null) {
+				sum++;
+			}
+		}
+		String[] sortArray = new String[sum];
+
+		for (int i = 0; i < string.length; i++) {
+			if (string[i] != null) {
+				for (int j = 0; j < sortArray.length; j++) {
+					if (sortArray[j] == null) {
+						sortArray[j] = string[i];
+						break;
+					}
+				}
+			}
+		}
+		Arrays.sort(sortArray);
+		System.out.println(Arrays.toString(sortArray));
+	}
+
+	public static void clear(String[] string) {
+		for (int i = 0; i < string.length; i++) {
+			string[i] = null;
+		}
+		System.out.println(Arrays.toString(string));
+	}
 
 }
