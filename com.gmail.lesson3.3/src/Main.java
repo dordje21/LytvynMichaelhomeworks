c75d9961d15f9c46513db9379a67ad4c108ef969
dordje21 <58385812+dordje21@users.noreply.github.com>
2019-12-23 16:53:46 +0200

    last

--- com.gmail.lesson3.3/src/Main.java
+++ com.gmail.lesson3.3/src/Main.java
@@ -3,23 +3,46 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String[] students = new String[15];
 
+		int[][] marks = new int[15][32];
+
+		int mark = 12;
+		int lesson = 2;
 		String st1 = "Andrey";
 		students[5] = "Vasy";
 		students[7] = "Kate";
 		students[11] = "Maks";
 		students[14] = "Masha";
 		addStudent(students, st1);
+		String name = "Kate";
+		setmark(marks, students, name, mark, lesson);
+		printmark(marks, students, name, lesson);
 		del(students, "Vasy");
 		print(students);
 		contains(students, "Andrey");
 		sortStudent(students);
 
 		System.out.println(Arrays.toString(students));
-		
+
 		clear(students);
+	}
+
+	public static void setmark(int[][] marks, String[] students, String name, int mark, int lesson) {
+		for (int i = 0; i < students.length; i++) {
+			if (students[i] == (name)) {
+				marks[i][lesson] = mark;
+				break;
+			}	
+		}System.out.println("добавили оценку " + mark + " " + name + " за занятие " + lesson);
+	}
+	
+	public static void printmark(int[][] marks, String[] students, String name, int lesson) {
+		for (int i = 0; i < students.length; i++) {
+			if (students[i] == (name)) {
+				System.out.println(name + " получила оценку за занятие" + " номер " + lesson + " = " + marks[i][lesson]);
+			}
+		}
 	}
 
 	public static void addStudent(String[] string, String st) {
