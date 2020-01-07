5eb4205a5276b7cb08be06684065dcd6eac60fb6
dordje21 <58385812+dordje21@users.noreply.github.com>
2020-01-07 20:53:42 +0200

    убрал пропуски в списке группы при выводе на печать

--- com.gmail.lesson3.3/src/Main.java
+++ com.gmail.lesson3.3/src/Main.java
@@ -57,9 +57,9 @@ public class Main {
 	public static void print(String[] string) {
 		for (int i = 0; i < string.length; i++) {
 			if (string[i] == null) {
-				System.out.print(" - ");
+				System.out.print("");
 			} else {
-				System.out.print(string[i]);
+				System.out.print(string[i] + " ");
 			}
 		}
 		System.out.println();
