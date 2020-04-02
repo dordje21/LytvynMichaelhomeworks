1988d38c04ea1034cf5348c921e8837cfb000f06
dordje21 <58385812+dordje21@users.noreply.github.com>
2020-04-02 20:54:49 +0300

    map

--- com,gmail.collections/src/Main.java
+++ com,gmail.collections/src/Main.java
@@ -1,7 +1,11 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeSet;
 
 public class Main {
@@ -9,37 +13,57 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-//		List<Integer> list = new ArrayList<>();
-//		
-//		list.add(4);
-//		list.add(14);
-//		list.add(24);
-//		list.add(34);
-//		list.add(44);	
-//		System.out.println(list);
-//		
-//		list.set(2, 45);	
-//		System.out.println(list);
-//		
-//		int position = list.indexOf(14);
-//		System.out.println(position);
-//		
-//		
-//		list.add(2, 46);
-//		System.out.println(list);
-//		
-//		list.remove(0);
-//		System.out.println(list);
-//		
-//		for(int i = 0; i <list.size(); i++) {
-//			System.out.println(list.get(i));
-//		}
-//		
-//		Iterator<Integer> itr = list.iterator();
-//		
-//		for(;itr.hasNext();) {
-//			System.out.println(itr.next());
-//		}
+		List<Integer> list = new ArrayList<>();
+		
+		list.add(4);
+		list.add(14);
+		list.add(24);
+		list.add(34);
+		list.add(44);	
+		System.out.println(list);
+		
+		Collections.shuffle(list);
+		
+		System.out.println(list);
+		
+		
+		List<Integer> listTwo = new ArrayList<>();
+		
+		listTwo.add(42);
+		listTwo.add(124);
+		listTwo.add(24);
+		listTwo.add(224);
+		listTwo.add(424);	
+		
+		int fr = Collections.frequency(listTwo, 24);
+		
+		System.out.println(listTwo + " сколько 24 в listTwo = " + fr);
+		
+		boolean dis = Collections.disjoint(list, listTwo);
+		System.out.println(dis);
+		
+		list.set(2, 45);	
+		System.out.println(list);
+		
+		int position = list.indexOf(14);
+		System.out.println(position);
+		
+		
+		list.add(2, 46);
+		System.out.println(list);
+		
+		list.remove(0);
+		System.out.println(list);
+		
+		for(int i = 0; i <list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		
+		Iterator<Integer> itr = list.iterator();
+		
+		for(;itr.hasNext();) {
+			System.out.println(itr.next());
+		}
 		
 		ArrayDeque<Integer> deq = new ArrayDeque<Integer>();
 		
@@ -72,6 +96,26 @@ public class Main {
 		for(;itertwo.hasNext();) {
 			System.out.println(itertwo.next());
 		}
+		
+		Map<Integer, String> number = new HashMap<Integer, String>();
+		
+		number.put(1, "one");
+		number.put(2, "two");
+		number.put(3, "three");
+		
+		System.out.println(number);
+		
+		String value= number.get(3);
+		System.out.println(value);
+		
+		System.out.println(number.get(2));
+		
+		number.put(5, "five");
+		System.out.println(number);
+		
+		String delete = number.remove(3);
+		System.out.println(delete);
+		System.out.println(number);
 	}
 
 }
